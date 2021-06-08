67c9acf5c7a5da81e4d60dadfb315989d5d5c54f
Bradley Reed <shenmusha@yahoo.com>
2021-06-08 17:14:30 -0400

    added classes

--- ReactWeek1Assignment.js
+++ ReactWeek1Assignment.js
@@ -1,3 +1,16 @@
-class Student {};
+class Student {
+    constructor(name, email, community) {
+        this.name = name,
+            this.email = email,
+            this.community = community
+    }
+};
 
-class Bootcamp {};
+class Bootcamp {
+    constructor(name, level, students) {
+        this.name = name;
+        this.level = level,
+            this.students = students
+    }
+
+};
